5e44984b13c04b848be74787aec7dd10e6ebd8cb
KIM SHINSEONG <moosewithbear@gmail.com>
2022-11-09 18:19:52 +0900

    [init] 리액트 공부 시작 20221109

---
+++ blog/log.js
@@ -0,0 +1,21 @@
+// node.js 최신버전 설치
+// 폴더 들어온 후터미널 열고
+//! npx create-react-app blog    
+// 설명 :
+// npx -> 설치를 도와주는 명령어 node.js에 포함돼있음
+// create-react-app -> 라이브러리 이름. 만들기 쉽게 세팅이 다 되어있는 boilerplate라고 한다.
+// blog-> 프로젝트 이름
+//! Success!, Happy hacking! 나오면 정상적으로 설치 된 것이다.
+// 생성된 폴더에서 src폴더의 App.js파일이 메인파일이고 = index.html이라고 보면된다.
+
+//! blog폴더를 아래 터미널로 맞춘 후 npm start -> http://localhost:3000/페이지 실행됨
+
+
+//^ public/index.html 파일이 실행되지만 안에는 아무것도 없음.
+//^ 이유는 index.js파일의 코드를 통해서 App.js코드가 index.html파일로 이동되기 때문
+//^ 즉, 실질적 코딩은 App.js, App.css 에서 하면 된다.
+//node_module 폴더 : 사이트를 구동하기위한 라이브러리를 모아놓은 폴더
+//public 폴더 : static 파일들을 모아놓은 폴더
+//package.json 파일 : 설치한 라이브러리 및 버전 목록의 기록 (건들필요 없지만 오류가 나는 경우 로그를 보고 참고 할 수 있다.)
+
+
