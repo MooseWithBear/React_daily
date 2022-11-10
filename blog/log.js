f26b5d7705e4e6bb5b38318142467326ac6ca7cf
KIM SHINSEONG <moosewithbear@gmail.com>
2022-11-10 18:13:50 +0900

    20221110

--- blog/log.js
+++ blog/log.js
@@ -1,3 +1,6 @@
+//!리액트의 대원칙 => immutable data
+
+
 // node.js 최신버전 설치
 // 폴더 들어온 후터미널 열고
 //! npx create-react-app blog    
@@ -25,3 +28,55 @@
 
 //! div안에 css를 직접 넣을 때는 아래와 같이 {}안에 {}를 넣고 camel방식을 사용한다. -> js문법이랑 충돌하지 않도록 하기 위함
 // <div style={ {color:'#61dafb', fontSize:'20px'} }> 개발 Blog </div> 
+
+
+//! 데이터를 저장하는 방법
+// 1. 변수에 보관 -> let = '강남 고기 맛집'; 으로 저장하고 불러오기
+//* 2. state에 보관 
+// -> import React,{ useState } from 'react'; 선언 후
+// -> useState('aaa')로 적었을 때 useState안에는 aaa이외의 값을 넣을 수 있는  Array[a,b]로 변한다.
+// (ES6 destructuring) var [a,b] = [10,100]; -> a와 b에 각각 10, 100의 값이 저장되는 문법
+// -> 이 문법을 활용하여 let [a,b] = useState(['A','B']); 로 작성한다면 a[0]='A', a[1]='B'가 된다.
+// -> 그리고 b는 함수(); 가 된다.
+//* states에 보관하는 이유? 
+//* states가 변경될 때 HTML이 자동으로 재렌더링(새로고침)이 된다. 그러므로 자주바뀌는 중요한 데이터는 변수보다는 state로 저장해서 쓰는 것이 좋다.
+
+
+//! state로 클릭시 숫자가 올라가는 기능 만들어보기
+//0.       <h3> { 글제목[0] } <span onClick={ ()=>{ console.log(1) } }>👍</span> 0</h3> //0부분을 state활용
+//1.  let [따봉, 따봉변경] = useState(0)
+
+
+
+//! state 수정 원칙 그리고 
+//1. state 초기값을 직접 건들지 않는다.
+//2. 대신 state 초기값을 새로운 변수에 저장하여 수정한다.
+
+
+// function 제목바꾸기() {
+     // 글제목변경(['여자 코트 추천', '강남 라면 맛집', '역삼 스타벅스 추천']);
+//     var newArray = [...글제목];
+     // var newArray = 글제목 으로 작성하면 값은 변경되지만 웹상에선 적용되지 않는데, 
+     // 스테이트가 deep copy되지 않은 채로 복사되어 newArray가 변경된 것이므로 state값이 변하지 않는다고 인식하여
+     // html이 새로고침되지 않는 것 같다.
+//     newArray[0] = '여자코트 추천';
+//     글제목변경( newArray )
+//   }
+  
+
+//! 컴포넌트(component)만드는 법
+// export default App 위에 아래 함수를 만든 후
+//{/* <Modal></Modal> */} 을 함수처럼 사용하면 된다.
+// function Modal() {
+//     return (
+//       <div className='modal'>
+//         <h2>제목</h2>
+//           <p>날짜</p> 
+//           <p>상세내용</p>
+//       </div>
+  
+//     )
+//   }
+
+//https://www.youtube.com/watch?v=bq2SjODrhJQ&list=PLfLgtT94nNq1e6tr4sm2eH6ZZC2jcqGOy&index=7
+//6강
--- blog/src/App.js
+++ blog/src/App.js
@@ -1,30 +1,71 @@
+import React, { useState } from 'react'; //추가
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let 제목 = '강남 고기 맛집'; //데이터 바인딩
-  let 로고명 = '리액트'
+  let [글제목, 글제목변경] = useState (['남자코트 추천', '강남 우동 맛집', '강남 스타벅스 추천']);
+  let [따봉, 따봉변경] = useState(0)
+  // let 제목 = '강남 고기 맛집'; //데이터 바인딩
+  let 로고명 = '리액트' 
   let 로고클래스 = 'logoImage' 
-  
+  // 글제목변경 (글제목[0]='여자코트추천') 
+function 제목바꾸기() {
+  // 글제목변경(['여자 코트 추천', '강남 라면 맛집', '역삼 스타벅스 추천']);
+  var newArray = [...글제목];
+  // var newArray = 글제목 으로 작성하면 값은 변경되지만 웹상에선 적용되지 않는데, 
+  // 스테이트가 deep copy되지 않은 채로 newArray가 변경된 것이므로 state값이 변하지 않는다고 인식하여
+  // html이 새로고침되지 않는 것 같다.
+  newArray[0] = '여자코트 추천';
+  newArray[1] = '강남 삼겹살 맛집';
+  newArray[2] = '역삼역 스타벅스 추천';
+
+  글제목변경( newArray )
+}
+
   return (
 <div className="App">
     <div className="black-nav">
     <img className={로고클래스} src={ logo } alt = {로고명} />
         <div style={ {color:'#61dafb'} }> 개발 Blog </div>
     </div>
+    <button onClick={ 제목바꾸기 }>제목변경버튼</button>
     <div className='list'>
-      <h3> { 제목 } </h3>
-      <p>11월 10일 발행</p>
+      <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉+1)  } }>👍</span> {따봉} </h3>
+      <p>11월 10일 발행</p> 
     <hr/>
     </div>
     <div className='list'>
-      <h3> { 제목 } </h3>
+      <h3> { 글제목[1] } </h3>
+      <p>11월 10일 발행</p>
+    <hr/>
+    </div>
+        <div className='list'>
+      <h3> { 글제목[2] } </h3>
       <p>11월 10일 발행</p>
     <hr/>
     </div>
 
+  
+<Modal></Modal>
+
 </div>
   );
 }
+
+
+function Modal() {
+  return (
+    <>
+    <div className='modal'>
+      <h2>제목</h2>
+        <p>날짜</p> 
+        <p>상세내용</p>
+    </div>
+    </>
+  )
+}
+
+
+
 
 export default App;
