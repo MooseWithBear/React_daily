cd4153e8e5c898a77204f0701db8ef4d34f67cd3
KIM SHINSEONG <moosewithbear@gmail.com>
2022-11-10 16:47:25 +0900

    20221110

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # React_daily
-daily study
+react daily study log
+source : 코딩애플 youtube https://www.youtube.com/watch?v=LclObYwGj90&list=PLfLgtT94nNq1e6tr4sm2eH6ZZC2jcqGOy&index=1
--- blog/src/App.js
+++ blog/src/App.js
@@ -2,23 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let 제목 = '강남 고기 맛집'; //데이터 바인딩
+  let 로고명 = '리액트'
+  let 로고클래스 = 'logoImage' 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 안녕
-        </a>
-      </header>
+<div className="App">
+    <div className="black-nav">
+    <img className={로고클래스} src={ logo } alt = {로고명} />
+        <div style={ {color:'#61dafb'} }> 개발 Blog </div>
     </div>
+    <div className='list'>
+      <h3> { 제목 } </h3>
+      <p>11월 10일 발행</p>
+    <hr/>
+    </div>
+    <div className='list'>
+      <h3> { 제목 } </h3>
+      <p>11월 10일 발행</p>
+    <hr/>
+    </div>
+
+</div>
   );
 }
 
